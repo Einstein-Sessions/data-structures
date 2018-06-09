80a992cf1df7d913f6d9341d5a5cfa15b8ed73ab
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-06-08 23:28:10 -1100

    add queue methods to implement

--- src/main/java/com/javasessions/queues/BasicQueue.java
+++ src/main/java/com/javasessions/queues/BasicQueue.java
@@ -2,12 +2,31 @@ package com.javasessions.queues;
 
 public class BasicQueue<T> {
     private T[] data;
+    private int front;
+    private int end;
 
     public BasicQueue() {
         this(1000);
     }
 
     public BasicQueue(int size) {
+        this.front = -1;
+        this.end = -1;
         data = (T[])new Object[size];
     }
+
+    public int size() {
+        // if the queue is empty return 0
+        if (this.front == -1 && this.end == -1) {
+            return 0;
+        }
+        // otherwise we get the inclusive subtraction value rather than
+        else {
+            return end - front + 1;
+        }
+    }
+
+    public void enQueue(T item) { }
+
+    public T deQueue() { }
 }
